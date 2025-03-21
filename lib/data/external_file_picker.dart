1c24b697125c4648d33d6b741945bb98cb09d287
Alexey Kuznetsov <a.kuznetsov@faqtrip.com>
2025-03-21 09:57:13 +0300

    photo resize

--- lib/data/external_file_picker.dart
+++ lib/data/external_file_picker.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
+import 'dart:ui' as ui;
 
-import 'package:file_picker/file_picker.dart' as file_picker;
 import 'package:flutter/cupertino.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:todo/domain/entity.dart';
 import 'package:todo/domain/i_file_picker.dart';
@@ -11,17 +12,20 @@ class ExternalFilePicker implements IFilePicker {
   @override
   Future<FilePickerResult> pickFile() async {
     try {
-      final result = await file_picker.FilePicker.platform.pickFiles(
-        type: file_picker.FileType.image,
+      final result = await ImagePicker().pickImage(
+        source: ImageSource.gallery,
+        maxWidth: 800,
+        imageQuality: 80,
       );
+
       if (result == null) return const FilePickerCancel();
 
-      final toCopy = File.fromUri(Uri.parse(result.files.first.identifier!));
-      final directory = await getApplicationDocumentsDirectory();
-      final path = '${directory.path}/${const Uuid().v4()}';
-      await toCopy.copy(path);
+      final copier = File(result.path);
+      final destinationPath =
+          '${(await getApplicationDocumentsDirectory()).path}/${const Uuid().v4()}';
+      await copier.copy(destinationPath);
 
-      return FilePickerSuccess(TodoLocalImageEntity(path));
+      return FilePickerSuccess(TodoLocalImageEntity(destinationPath));
     } catch (e) {
       debugPrint('Error: $e');
       return FilePickerFailure(e.toString());
